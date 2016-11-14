3ee55b7f6c7b38065179f057b6d5f54a1bab14eb
Bryan Allen <Bryan Allen>
2016-11-13 21:56:35 -0600

    my version

--- Server.java
+++ Server.java
@@ -1,69 +1,85 @@
-//Iris Osegueda
 import java.io.*;
-import java.net.*;
 import java.util.*;
+import java.net.*;
+import java.util.concurrent.*; //have to do it separetely bc it isn't a class 
+//we know bc its not capitalized
 
-public class Server
+public class ServerMT 
 {
 	private static ServerSocket serverSocket;
 	private static Socket connection = null;
-	
-	public static void main(String[] args)
-	{
-		Scanner input = null;
-		Formatter output = null;
-		String message = "";
-		String response = "";
-		try
+
+	public static void main(String args[])
+	{		
+		try 
 		{
 			serverSocket = new ServerSocket(8000, 100);
-			while(true) 
+			
+			int clientNo = 1;
+			
+			ExecutorService threadExecutor = Executors.newCachedThreadPool();
+			while(true)
 			{
-				try
-				{
-					System.out.println("waiting for connection");
-					connection = serverSocket.accept();
-
-					System.out.println("Client has connected");
-					input = new Scanner(connection.getInputStream());
-					output = new Formatter(connection.getOutputStream());
-
-					System.out.println("waiting for input");
-					while(true)
-					{
-						message = input.next() + "\n";
-
-						System.out.println("Received message: " + message);
-						response = Integer.toString(message.length() - 1) + "\n";
-						System.out.println("Returning length: " + response);
-						
-						output.format(response);
-						output.flush();
-						System.out.println("Sent response" + "\n");
-					}
-				}
-				catch(IOException ioe)
-				{
-					ioe.printStackTrace();
-				}
+				connection = serverSocket.accept();
+				System.out.println("Start thread for client " + clientNo);
+				
+				HandleAClient thread = new HandleAClient(connection, clientNo);
+				
+				threadExecutor.execute(thread);
+				
+				clientNo++;
+				//everytime a client connects with us we're going to give them a number
+				//and start their own thread 
 			}
 		}
 		catch(IOException ioe)
 		{
 			ioe.printStackTrace();
 		}
-		finally
+	}
+}
+
+//everytime a client connects with us we're going to give them a number
+//and start their own thread 
+class HandleAClient implements Runnable
+{
+	private Scanner input;
+	private Formatter output;
+	private Socket connection;
+	private int clientNo;
+	private String message = "";
+	private String response = "";
+	
+	public HandleAClient(Socket connection, int clientNo)
+	{
+		this.connection = connection;
+		this.clientNo = clientNo;
+		
+	}
+	public void run()
+	{
+		try
 		{
-			try
+			System.out.println("waiting for input");
+			input = new Scanner(connection.getInputStream());
+			output = new Formatter(connection.getOutputStream());
+			
+			while(input.hasNext())
 			{
-				output.close();
-				input.close();
-				connection.close();
+				message = input.next() + "\n";
+
+				System.out.println("Received message from client " + this.clientNo + ": " + message);
+				response = Integer.toString(message.length() - 1) + "\n";
+				System.out.println("Returning length: " + response);
+				
+				output.format(response);
+				output.flush();
 			}
-			catch(IOException ioe)
-			{
-				ioe.printStackTrace();
-			}
+			System.out.println("thread " + clientNo + " ended");
+		}
+		catch(IOException ioe)
+		{
+			ioe.printStackTrace();
 		}
 	}
 }
